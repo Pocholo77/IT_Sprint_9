61de8db8e5250f1620bf9bbb71e2cf98d811fa38
Eduardo Wiater <kpooh7766@gmail.com>
2021-10-23 21:11:31 +0200

    SearchBar Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Youtube from "./api/Youtube";
+import SearchBar from "./components/SearchBar/SearchBar";
+ 
 
 export default function App() {
 
@@ -9,13 +11,16 @@ export default function App() {
   })
   //«handleSubmit» i «handleVideoSelect»:
   async function handleSubmit(e){
+    e.preventDefault()
+    //console.log(e.target.elements[0].value);
+ 
     const response = await Youtube.get('/search',{
       params:{
         q: e,
       }
     })
   }
-  
+
   function handleVideoSelect(){
 
   }
@@ -24,6 +29,7 @@ export default function App() {
 
     <div className="App">
       <h1>App</h1>
+      <SearchBar handleSubmit={handleSubmit}/>
     </div>
   )
 }
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SearchBar({handleSubmit}){
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Search'></input>
+            <button type='submit'>Search</button> {/* button */}
+            </form>
+        </div>
+    )
+}
