cddb4855ae428e3b47c156bd344d082d42ce0e4c
Eduardo Wiater <kpooh7766@gmail.com>
2021-10-24 02:11:19 +0200

    Feat: Fix SearchBar function

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,7 +5,7 @@ export default function SearchBar({handleSubmit}){
     return(
         <div>
             <form onSubmit={handleSubmit}>
-            <input type='text' placeholder='Search' onChange={handleSubmit}></input>
+            <input type='text' placeholder='Search' ></input>
             <button type='submit'>Search</button> {/* button */}
             </form>
         </div>
