c7d6a5d1caf970c6e29ad51e1a67f364ed381cbc
Eduardo Wiater <kpooh7766@gmail.com>
2021-10-28 00:26:34 +0200

    Feat: create SearchPage & its css

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,11 +1,15 @@
 import React from "react";
+import AsideNav from "../../components/AsideNav/AsideNav";
 import VideoList from "../../components/VideoList/VideoList";
-import './SearchPage.css'
+import "./SearchPage.css";
 
 export default function SearchPage(props) {
   return (
-    <div>
-      <VideoList {...props} />
+    <div className="searchPage__container">
+      <AsideNav />
+      <div className="searchPage__videos-container">
+        <VideoList {...props} />
+      </div>
     </div>
   );
 }
