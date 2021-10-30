df07dfaaa36b7138290b5fce876bbb33e4a37012
Eduardo Wiater <kpooh7766@gmail.com>
2021-10-30 20:56:20 +0200

    Feat: Create & style AsideNav component

--- src/components/AsideNav/AsideNav.js
+++ src/components/AsideNav/AsideNav.js
@@ -1,22 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './AsideNav.css'
 
 export default function AsideNav() {
   return (
-    <aside>
+    <aside className="asideNav-container">
       <nav>
         <ul>
           <li>
-            <Link to="/">home</Link>
+            <Link to="/">
+              <div className="asideNav__link-container">
+                <div><img style={{height:"12px", marginLeft:"12px"}} src="./img/home.png" alt=""/></div>
+                <div className="asideNav__link__name"><h5><p>Inicio</p></h5></div> 
+              </div>
+            </Link>
           </li>
           <li>
-            <Link to="/favourites">favorito</Link>
+          <Link to="/favourites">
+              <div className="asideNav__link-container">
+                <div className="asideNav__link__icon"><img src="./img/liked.png" alt=""/></div>
+                <div className="asideNav__link__name"><h5><p>Favoritos</p></h5></div> 
+              </div>
+            </Link>
           </li>
           <li>
-            <Link to="/history">history</Link>
+          <Link to="/history">
+              <div className="asideNav__link-container">
+                <div className="asideNav__link__icon"><img src="./img/history.png" alt=""/></div>
+                <div className="asideNav__link__name"><h5><p>Historial</p></h5></div> 
+              </div>
+            </Link>
           </li>
         </ul>
       </nav>
+      <hr />
+      
     </aside>
   );
 }
