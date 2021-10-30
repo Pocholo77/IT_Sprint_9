c8c1135a341b056d6f6f8a32e9048483b6d87da2
Eduardo Wiater <kpooh7766@gmail.com>
2021-10-31 00:40:59 +0200

    Feat: Create videoitemHome

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,19 +1,37 @@
 import React from "react";
-import './VideoList.css'
+import "./VideoList.css";
 import VideoItem from "../VideoItem/VideoItem";
+import VideoItemHome from "../VideoItemHome/VideoItemHome";
 
-
-
-export default function VideoList({ videos, handleVideoSelect, favourites, handleFavourite, type="list"}) {
+export default function VideoList({
+  videos,
+  handleVideoSelect,
+  favourites,
+  handleFavourite,
+  type = "list",
+}) {
   /* console.log(videos) */
   return (
     <div className="videoList-container">
       <ul className={type}>
         {videos.map((vid, index) => {
           return (
- 
             <li key={`${vid.id.videoId}${index}`}>
-              <VideoItem item={vid} onClick={handleVideoSelect} favourites={favourites} handleFavourite={handleFavourite}/>
+              {type === "list" ? (
+                <VideoItem
+                  item={vid}
+                  onClick={handleVideoSelect}
+                  favourites={favourites}
+                  handleFavourite={handleFavourite}
+                />
+              ) : (
+                <VideoItemHome
+                  item={vid}
+                  onClick={handleVideoSelect}
+                  favourites={favourites}
+                  handleFavourite={handleFavourite}
+                />
+              )}
               {/* <img src={vid.snippet.thumbnails.default.url} alt=""/> */}
             </li>
           );
